ae2ac0c7b00aad214fbb84b659ee5b365bf539ae
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-24 16:21:38 -0400

    calculation of dollars to Colombian pesos

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -5,3 +5,9 @@ dolares = pesos / valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
 print("Tienes $"+ dolares + " dólares")
+
+dolares = input("¿cuantos dolares tienes?: ")
+dolares = float(dolares)
+valor_peso_colombiano = 3875
+pesos = dolares*valor_peso_colombiano
+print("Tienes COP "+ str(pesos))
