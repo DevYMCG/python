e2f63efd9e25a252c248789f500483707df4819b
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-24 17:30:49 -0400

    conditional to calculate from dollars to pesos

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -9,3 +9,13 @@ dolares = float(dolares)
 valor_peso_colombiano = 3875
 pesos = dolares*valor_peso_colombiano
 print("Tienes COP "+ str(pesos)+ " colombianos")
+
+opcion = input("¿Deseas: opc1: Pesos-Dolares opc2: Dolares-pesos? (Responde 1 o 2) ")
+if opcion == "opc1":
+    pesos = float(input("¿Cuántos pesos colombianos tienes?: "))
+    valor_dolar = 3.800
+    print("Tienes $"+ str(round(pesos / valor_dolar, 2)) + " dólares. ")
+elif opcion == "opc2":
+    dolares = float(input("¿Cuántos dolares tienes?: "))
+    valor_peso = 3.800
+    print("Tienes COP"+ str(round(dolares / valor_peso, 2)) + " pesos. ")
