5cb90a7fce4db85de73853d245909668a09e40d2
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-24 16:27:17 -0400

    debugging code the conversor

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -4,8 +4,8 @@ valor_dolar = 3875
 dolares = pesos / valor_dolar
 print("Tienes $"+ str(round(dolares, 2)) + " dólares")
 
-dolares = input("¿cuantos dolares tienes?: ")
+dolares = input("¿Cuántos dolares tienes?: ")
 dolares = float(dolares)
 valor_peso_colombiano = 3875
 pesos = dolares*valor_peso_colombiano
-print("Tienes COP "+ str(pesos))
+print("Tienes COP "+ str(pesos)+ " colombianos")
