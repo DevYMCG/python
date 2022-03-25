0544e92a13923e7d9eb893d7c272386c2bb92b3b
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-24 20:02:30 -0400

    functions

---
+++ codigo_python/funciones.py
@@ -0,0 +1,23 @@
+# def imprimir_mensaje():
+#     print('Mensaje especial: ')
+#     print('Estoy aprendiendo a usar funciones')
+
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje() 
+
+def conversacion(mensaje):
+    print('Hola')
+    print('Cómo estás')
+    print(mensaje)
+    print('Adios')
+
+opcion = int(input('Elige una opción(1, 2, 3): '))
+if opcion == 1:
+    conversacion('Elegistes la opción 1')
+elif opcion == 2:
+    conversacion('Elegistes la opción 2')
+elif opcion == 3:
+    conversacion('Elegistes la opción 3')
+else:
+    print('Ingrese una opción correcta por favor')
