98436ea8fc45593b5ab892b9c446e9e971d55a9c
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-25 18:43:24 -0400

    looping through the dictionary by key and value

--- codigo_python/diccionarios.py
+++ codigo_python/diccionarios.py
@@ -7,8 +7,14 @@ def run():
 
     # print(poblacion_paises['Argentina'])
 
-    for pais in poblacion_paises.keys():
-        print(pais)
+    # for pais in poblacion_paises.keys():
+    #     print(pais)
+
+    # for pais in poblacion_paises.values():
+    #     print(pais)
+
+    for pais, poblacion in poblacion_paises.items():
+        print(pais + ' tiene '+str(poblacion)+ 'habitantes')
 
 if __name__ == '__main__':
     run()
