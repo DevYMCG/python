d4fd32429e2ea6f9c0cfe75be43ab4c43c850a97
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-25 18:27:45 -0400

    dictionary

---
+++ codigo_python/diccionarios.py
@@ -0,0 +1,11 @@
+def run():
+    poblacion_paises = {
+        'Argentina': 44938712,
+        'Brasil': 210147125,
+        'Colombia': 50372424
+    }
+
+    print(poblacion_paises['Argentina'])
+
+if __name__ == '__main__':
+    run()
