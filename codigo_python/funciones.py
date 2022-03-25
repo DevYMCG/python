a8dd2180f5e66c536a9c191bb01d6469d3864a53
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-24 20:19:24 -0400

    return in a function

--- codigo_python/funciones.py
+++ codigo_python/funciones.py
@@ -6,18 +6,26 @@
 # imprimir_mensaje()
 # imprimir_mensaje() 
 
-def conversacion(mensaje):
-    print('Hola')
-    print('Cómo estás')
-    print(mensaje)
-    print('Adios')
+# def conversacion(mensaje):
+#     print('Hola')
+#     print('Cómo estás')
+#     print(mensaje)
+#     print('Adios')
 
-opcion = int(input('Elige una opción(1, 2, 3): '))
-if opcion == 1:
-    conversacion('Elegistes la opción 1')
-elif opcion == 2:
-    conversacion('Elegistes la opción 2')
-elif opcion == 3:
-    conversacion('Elegistes la opción 3')
-else:
-    print('Ingrese una opción correcta por favor')
+# opcion = int(input('Elige una opción(1, 2, 3): '))
+# if opcion == 1:
+#     conversacion('Elegistes la opción 1')
+# elif opcion == 2:
+#     conversacion('Elegistes la opción 2')
+# elif opcion == 3:
+#     conversacion('Elegistes la opción 3')
+# else:
+#     print('Ingrese una opción correcta por favor')
+
+def suma(a, b):
+    print('Se suman dos números')
+    resultado= a + b;
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
