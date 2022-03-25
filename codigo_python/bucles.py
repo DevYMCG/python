143de98da47b4252edfd4f9c524169841bd672ea
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-25 09:47:30 -0400

    power from 0 to 1000 using the while loop

--- codigo_python/bucles.py
+++ codigo_python/bucles.py
@@ -1,10 +1,12 @@
-def run(exponente):
-    if exponente < 1000:
-        print("2^"+str(exponente)+" = "+str(2**exponente))
-        run(exponente+1)
-    else:
-        print("Fin!!")
+def run():
+    LIMITE = 1000
+
+    contador = 0
+    potencia_2 = 2**contador
+    while contador < LIMITE:
+        print('2 elevado a '+str(contador)+" es igual a: "+str(2**contador))
+        contador += 1
 
 
 if __name__ == '__main__':
-    run(1)
+    run()
