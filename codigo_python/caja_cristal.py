b94a763f1be8f9e0b1b1e4827c49cf54927aaee9
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-26 22:19:59 -0400

    unit tests - improvements

--- codigo_python/caja_cristal.py
+++ codigo_python/caja_cristal.py
@@ -9,14 +9,14 @@ def es_mayor_de_edad(edad):
 
 class PruebaDeCristalTest(unittest.TestCase):
 
-    def test_es_mayor(self):
+    def test_es_mayor_de_edad(self):
         edad = 30
 
         resultado = es_mayor_de_edad(edad)
 
         self.assertEqual(resultado, True)
     
-    def test_es_menor(self):
+    def test_es_menor_de_edad(self):
         edad = 13
 
         resultado = es_mayor_de_edad(edad)
