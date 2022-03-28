906f338ea715a8da8a2d107e4e2ba531fe5ca87b
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-28 18:28:22 -0400

    use of order as higher order function

--- curso-python/high_order_functions.py
+++ curso-python/high_order_functions.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import math
 
 def run():
@@ -10,15 +11,25 @@ def run():
     # list_numeros_impares = list(filter(lambda x: x%2 != 0, my_list))
 
 
-    my_list = [1,2,3,4,5]
+    # my_list = [1,2,3,4,5]
 
     # list = [i**2 for i in my_list] 
 
     # uso de map
     
-    squares = list(map(lambda x: x**2, my_list))
+    # squares = list(map(lambda x: x**2, my_list))
 
-    print(squares)
+    #uso de reduces
+
+    my_list = [2,2,2,2,2]
+    # acum = 1
+
+    # for value in my_list:
+    #     acum= acum*value;
+
+    all_multiplied = reduce(lambda a, b: a * b, my_list)
+
+    print(all_multiplied)
 
 
 if __name__ == '__main__':
