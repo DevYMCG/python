8283d18fe1bf00eea1184fd602626d63bbb57922
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-28 16:58:04 -0400

    challenge

--- curso-python/Dictionary_comprehensions.py
+++ curso-python/Dictionary_comprehensions.py
@@ -1,3 +1,5 @@
+import math
+
 def run():
     # my_dict = {}
 
@@ -5,6 +7,15 @@ def run():
     #     my_dict[i] = i**3
 
     my_dict = {i: i**3 for i in range(1, 101) if i%3!=0}
+
+    """
+        Crear, un dictionary comprehension, un 
+        diccionario cuyas llaves sean los 1000
+        primeros números naturales con sus raíces
+        cuadradas como valores
+    """
+
+    my_dict = {i: round(math.sqrt(i),2) for i in range(1, 1001)}
     
     print(my_dict)
 
