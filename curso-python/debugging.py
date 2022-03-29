409fa9bc54c10eae96df7c6e69f58d1949d19ed1
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-28 23:04:45 -0400

    adding exceptions

--- curso-python/debugging.py
+++ curso-python/debugging.py
@@ -1,15 +1,26 @@
 def divisors(num):
     divisors = []
-    for i in range(1, num +1):
-        if num%i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        if num < 1:
+            raise ValueError("No debes ingresar números negativos")
+        for i in range(1, num +1):
+            if num%i == 0:
+                divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        print(ve)
+
 
 
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Terminó mi programa")
+    try:
+        num = int(input("Ingresa un número: "))
+        print(divisors(num))
+        print("Terminó mi programa")
+    except ValueError:
+        print("Debes ingresar un números")
+
+
 
 if __name__ == '__main__':
     run()
