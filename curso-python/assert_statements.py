006557c0c7143ed5b2480b41e829e536ea63103a
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-03-30 11:26:49 -0400

    adding exceptions to the code

--- curso-python/assert_statements.py
+++ curso-python/assert_statements.py
@@ -1,15 +1,9 @@
 def divisors(num):
     divisors = []
-    try:
-        if num < 1:
-            raise ValueError("No debes ingresar números negativos")
-        for i in range(1, num +1):
-            if num%i == 0:
-                divisors.append(i)
-        return divisors
-    except ValueError as ve:
-        print(ve)
-        return False
+    for i in range(1, num +1):
+        if num%i == 0:
+            divisors.append(i)
+    return divisors
 
 
 
