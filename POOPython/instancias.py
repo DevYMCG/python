6dc6bf1d1e5aec27db1dbf1efd4fb246ab2d01fd
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-01 17:00:46 -0400

    improvements in instancias.py

--- POOPython/instancias.py
+++ POOPython/instancias.py
@@ -1,7 +1,7 @@
 class Coordenada:
     def __init__(self, x, y):
         self.x = x
-        self.y = y;
+        self.y = y
     
     def distancia(self, otra_coordenada):
         x_diff = (self.x - otra_coordenada.x)**2
