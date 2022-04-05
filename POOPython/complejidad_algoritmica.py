e7d7bcaf9a90403b2dd7b0b03c71945297bcae9e
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-05 09:06:52 -0400

    recursion and linear search

--- POOPython/complejidad_algoritmica.py
+++ POOPython/complejidad_algoritmica.py
@@ -1,11 +1,14 @@
 import time
+import sys
+
+sys.setrecursionlimit(10000000)
 
 def factorial(n):
-    respuesta = 1
+    respuesta = 1 
 
-    while n>1:
+    while n>1: 
         respuesta *= n
-        n -=1
+        n -=1 
     
     return respuesta
 
@@ -13,10 +16,10 @@ def factorial_r(n):
     if n==1:
         return 1
     
-    return n* factorial(n-1)
+    return n* factorial_r(n-1)
 
 if __name__ == '__main__':
-    n = 200000
+    n = 10000
     
     comienzo = time.time()
     factorial(n)
