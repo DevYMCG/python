d71e739d187011c8bd8c5a2619a8f14bd4256dc2
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-19 16:19:25 -0400

    reto mejoras en codigo

--- estructura_de_datos_lineales_con_python/reto_array.py
+++ estructura_de_datos_lineales_con_python/reto_array.py
@@ -3,28 +3,24 @@
     2. Incorpora un método para poblar sus slots con números aleatorios o secuenciales
     3. Incluye un método que sume todos los valores del array.
 """
+from functools import reduce
 import random 
 
 class Array:
 
     def __init__(self, capacity, fill_value=None):
-        self.items = list()
-        for i in range(capacity):
-            numero_aleatorio = random.randint(1,100)
-            self.items.append(numero_aleatorio)
-    
-    def __suma__(self):
-        acum = 0
-        for i in range(5):
-            acum += self.items[i]
-        
-        return acum
+        self.items = [random.randint(1,100) for i in range(capacity)]
+
+    def __sumItems__(self):
+        """returns the sum of all the values ​​in the array"""
+        return reduce(lambda a, b: a+b, self.items)
 
     def __str__(self):
+        """Returns string representation of the array"""
         return str(self.items)
 
 if __name__ == "__main__":
     dimension = int(input('¿Ingresa la dimensión del array?: '))
     menu = Array(5)
     print(menu.__str__())
-    print(menu.__suma__())
+    print(menu.__sumItems__())
