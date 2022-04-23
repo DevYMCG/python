3c90d6a51b76d6e88d142e97a1af367d1f06cd68
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-23 12:02:51 -0400

    make_repeater_of usando clousures

--- curso_profesional_python/closures/clousures.py
+++ curso_profesional_python/closures/clousures.py
@@ -1,3 +1,15 @@
-def main(palabra: str, numero: int):
-    for i in range(numero):
-        print(palabra)
+def make_repeater_of(n):
+    def repeater(string):
+        assert type(string) == str, "solo puedes utilizar cadenas"
+        return string * n
+    return repeater
+
+def run():
+    repeat_5 = make_repeater_of(5)
+    repeat_7 = make_repeater_of(7)
+
+    print(repeat_5("hola"))
+    print(repeat_7("Yoha"))
+
+if __name__ == '__main__':
+    run()
