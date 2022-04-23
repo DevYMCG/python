c422d1a26a5a62868fbe0f21a6fafbe198f97ef7
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-23 12:12:50 -0400

    division mejoras

--- curso_profesional_python/closures/division.py
+++ curso_profesional_python/closures/division.py
@@ -1,13 +1,20 @@
-def make_division_by(n):
-    def division(m):
-        assert type(m) == int, "solo puedes utilizar números"
-        return m / n
+def make_division_by(x):
+    """This clousure returns a function that returns the division 
+        of an x number by n
+    """
+    def division(n):
+        assert type(n) == int, "solo puedes utilizar números"
+        return n / x
     return division
 
 def run():
     division_by_3 = make_division_by(3)
+    division_by_5 = make_division_by(5)
+    division_by_18 = make_division_by(18)
 
     print(division_by_3(18))
+    print(division_by_5(100))
+    print(division_by_18(54))
 
 if __name__ == '__main__':
     run()
