ada29c96c4020386b367075ab22987a6ce0b344b
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-04-26 21:16:39 -0400

    agregando la bienevenida al main

--- Creación_de_un_CRUD/main.py
+++ Creación_de_un_CRUD/main.py
@@ -4,8 +4,11 @@ def create_client(client_name):
     """definimos la variable global para poder usar la variable"""
     global clients
 
-    clients += client_name
-    _add_coma()
+    if client_name not in clients:
+        clients += client_name
+        _add_coma()
+    else:
+        print('Client already is in the client\'s list')
 
 
 def list_client():
@@ -18,9 +21,28 @@ def _add_coma():
 
     clients += ', '
 
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
 
 if __name__ == '__main__':
-    list_client()
-    create_client('David')
-    list_client()
+    _print_welcome()
+
+    command = input()
+
+    if command == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_client()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
+
+    
+
     
