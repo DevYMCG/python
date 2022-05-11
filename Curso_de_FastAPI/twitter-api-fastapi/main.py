fbef046a96c691dddc3075b64aca4737c8e2b2db
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-05-11 19:38:57 -0400

    tweet_post

--- Curso_de_FastAPI/twitter-api-fastapi/main.py
+++ Curso_de_FastAPI/twitter-api-fastapi/main.py
@@ -49,14 +49,15 @@ class UserRegister(User):
 
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
-    content: str = Field(...,
-    min_length=1, 
-    max_length=256
+    content: str = Field(
+        ...,
+        min_length=1, 
+        max_length=256
     )
     created_at: datetime = Field(
         default=datetime.now()
     )
-    update_at: Optional[date] = Field( default=None)
+    updated_at: Optional[datetime] = Field( default=None)
     by: User = Field(...)
 
 # Path Opertion
@@ -141,7 +142,7 @@ def show_all_users():
     with open("users.json", "r", encoding="utf-8") as f:
         results = json.loads(f.read())
         return results
-        
+
 
 ### show a user
 @app.get(
@@ -197,8 +198,33 @@ def show_all_tweets():
     summary="Post a tweet",
     tags=["Tweets"]   
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a Tweet
+    
+    This path operation post a tweet in the app
+
+    Parameters:
+    - Request body parameters:
+        - tweet: Tweet
+
+    Returns a json with the basic tweet information:
+        tweet_id: UUID
+        content: str
+        created_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])    
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @app.get(
